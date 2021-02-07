262d7894f11933d6674d94aec11d040a582a84c8
flokamp <74511633+flokamp@users.noreply.github.com>
2021-02-07 16:26:37 -0600

    license badges

--- Develop/index.js
+++ Develop/index.js
@@ -8,28 +8,43 @@ const questions = [
   {
     name: 'name',
     type: 'input',
-    message: 'What is your project title?',
+    message: 'What is the title of your project?',
   },
   {
     name: 'description',
     type: 'input',
-    message: 'What is your project description?',
-  },
-  {
-    name: 'license',
-    type: 'list',
-    message: 'How is this project licensed?',
-    choices: ['BSD', 'MIT', 'GPL'],
+    message: 'What is a description of your project?',
   },
   {
     name: 'installation',
     type: 'input',
-    message: 'What are the instructions for installing?',
+    message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
   },
   {
     name: 'usage',
     type: 'input',
-    message: 'Describe the usage.',
+    message: 'Provide instructions and examples for use. Include screenshots as needed.'
+  },
+  {
+    name: 'credits',
+    type: 'input',
+    message: 'List any collaborators with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence. If you followed tutorials, include those links.'
+  },
+  {
+    name: 'license',
+    type: 'list',
+    message: 'The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use https://choosealicense.com/',
+    choices: ['Apache License 2.0', 'Boost Software License 1.0', 'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'MIT License', 'Mozilla Public License 2.0', 'Unlicense', 'No License'],
+  },
+  {
+    name: 'contribution',
+    type: 'input',
+    message: 'If you would like other developer to contribute, add guidelines for how to do so.'
+  },
+  {
+    name: 'tests',
+    type: 'input',
+    message: 'Write tests for your application and provide examples on how to run them.'
   },
 ];
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,25 +1,69 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 const renderLicenseBadge = license => {
-  return `${license}`
+  if (license === 'Apache License 2.0') {
+    let license = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    return license;
+  }
+  else if (license === 'Boost Software License 1.0') {
+    let license = `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+    return license;
+  }
+  else if (license === 'GNU AGPLv3') {
+    let license = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0`
+    return license;
+  }
+  else if (license === 'GNU GPLv3') {
+    let license = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    return license;
+  }
+  else if (license === 'GNU LGPLv3') {
+    let license = `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`
+    return license;
+  }
+  else if (license === 'MIT License') {
+    let license = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    return license;
+  }
+  else if (license === 'Mozilla Public License 2.0') {
+    let license = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    return license;
+  }
+  else if (license === 'Unlicense') {
+    let license = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+    return license;
+  }
+  
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-const renderLicenseLink = license => {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-const renderLicenseSection = license =>  {}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = data => {
   const license = data.license
 
-return `## License
+return `# ${data.name}
 
-${renderLicenseBadge(license)}`
+${renderLicenseBadge(license)}
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+## License
+${data.license}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+`
 }
 
 module.exports = generateMarkdown;
